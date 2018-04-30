2dca15f8b7ece1fcf4a2eeee2fd1594f4c33d663
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-30 22:00:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, date_hash|
   date_hash.collect do |holiday, supplies|
     supplies.include?("BBQ") ? holiday:false
-    supplies.flatten
+    holiday.flatten
   end
   end
 end
