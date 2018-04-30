17fe440150170b926f58b60bd3d1f47839106e8f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-30 18:51:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize!}:"
   date_hash.each do |holiday, supplies|
     if holiday.to_s.include?("_")
-      string = holiday.to_s.split("_").to_s.capitalize
+      string = holiday.to_s.split("_").to_s.capitalize!
       puts "#{string}: #{supplies.join(", ")}"
     else
       puts "  #{holiday.to_s.capitalize!}: #{supplies.join(", ")}"
