88fbaec28a5fa892a4caa6b1a0929b9668a0cc81
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-30 20:38:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,9 +74,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, date_hash|
   date_hash.each do |holiday, supplies|
-  supplies.keep_ifinclude?("BBQ")
+  supplies.keep_if.include?("BBQ")
       
-    end
+   
   end
 end
 end
